293c7cd28fa445bef3e358fa1854a957e50a56b7
saurabhsatasia <saurabhsatasia@icloud.com>
2021-02-13 10:05:06 +0100

    update
    data transform

---
+++ DataTypeValidation_Insertion_Training/DataTypeValidation.py
@@ -0,0 +1,11 @@
+import shutil
+import sqlite3
+from datetime import datetime
+from os import listdir
+import os
+import csv
+from application_logging.logger import App_Logger
+
+class dbOperation:
+    def __init__(self):
+        pass
