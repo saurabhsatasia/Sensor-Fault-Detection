6e8f1bd0a20b71f271e4a913be498616b8a52b3f
saurabhsatasia <saurabhsatasia@icloud.com>
2021-02-20 03:07:57 +0100

    Update data_loader.py

--- data_ingestion/data_loader.py
+++ data_ingestion/data_loader.py
@@ -9,13 +9,11 @@ class Data_Getter:
     def get_data(self):
         self.logger_object.log(self.file_object,"Entered get_data method of the Data_Getter class")
         try:
-            self.data = pd.read_csv(self.training_file,"Data Load Successfull. Exited the get_data method of the Data_Getter class")
+            self.data = pd.read_csv(self.training_file)  # reading the data file
+            self.logger_object.log(self.file_object, 'Data Load Successful.Exited the get_data method of the Data_Getter class')
             return self.data
 
         except Exception as e:
-            self.logger_object.log(self.file_object,
-                                   'Exception occured in get_data method of the Data_Getter class. Exception message: ' + str(
-                                       e))
-            self.logger_object.log(self.file_object,
-                                   'Data Load Unsuccessful.Exited the get_data method of the Data_Getter class')
+            self.logger_object.log(self.file_object, 'Exception occured in get_data method of the Data_Getter class. Exception message: ' + str(e))
+            self.logger_object.log(self.file_object, 'Data Load Unsuccessful.Exited the get_data method of the Data_Getter class')
             raise Exception()
